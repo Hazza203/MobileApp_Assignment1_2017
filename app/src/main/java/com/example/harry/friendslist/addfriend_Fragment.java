2a357fc0a5f52c1369bbe10fcc6d1d94c61cf54b
JayMales <s3486717@student.rmit.edu.au>
2017-09-04 05:17:17 +1000

    Cleaned up add friend, a lot

--- app/src/main/java/com/example/harry/friendslist/addfriend_Fragment.java
+++ app/src/main/java/com/example/harry/friendslist/addfriend_Fragment.java
@@ -1,18 +1,8 @@
 package com.example.harry.friendslist;
 
-import android.Manifest;
-import android.annotation.SuppressLint;
-import android.content.pm.PackageManager;
+import android.content.ContentResolver;
 import android.database.Cursor;
-import android.net.Uri;
-import android.os.Build;
-import android.support.annotation.NonNull;
-import android.support.annotation.RequiresApi;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.LoaderManager;
-import android.support.v4.content.CursorLoader;
-import android.support.v4.content.Loader;
-import android.support.v4.widget.SimpleCursorAdapter;
 import android.widget.AdapterView;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
@@ -24,63 +14,32 @@ import android.provider.ContactsContract;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import com.example.harry.friendslist.model.Friend;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+
 
 /**
  * Created by harry on 1/09/2017.
  */
 
-public class addfriend_Fragment extends Fragment implements LoaderManager.LoaderCallbacks<Cursor>,
+public class addfriend_Fragment extends Fragment implements
         AdapterView.OnItemClickListener {
 
-    private static final int CONTACT_KEY_INDEX = 0;
     private String LOG_TAG = this.getClass().getName();
 
     private final static int[] TO_IDS = {
             android.R.id.text1
     };
+    private static final String TAG = addfriend_Fragment.class.getSimpleName();
 
-    ListView mContactsList;
+    private ListView mContactsList;
 
-    long mContactId;
 
-    String mContactKey;
+    private LinkedList<Friend> friends = new LinkedList<>();
 
-    Uri mContactUri;
 
-    private static final int CONTACT_ID_INDEX = 0;
-
-    private static final int LOOKUP_KEY_INDEX = 1;
-
-    private SimpleCursorAdapter mCursorAdapter;
-
-    @SuppressLint("InlinedApi")
-    private static final String SELECTION =
-            Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB ?
-                    ContactsContract.Contacts.DISPLAY_NAME_PRIMARY + " LIKE ?" :
-                    ContactsContract.Contacts.DISPLAY_NAME + " LIKE ?";
-    // Defines a variable for the search string
-    private String mSearchString;
-    // Defines the array to hold values that replace the ?
-    private String[] mSelectionArgs = { mSearchString };
-    @SuppressLint("InlinedApi")
-    private final static String[] FROM_COLUMNS = {
-            Build.VERSION.SDK_INT
-                    >= Build.VERSION_CODES.HONEYCOMB ?
-                    ContactsContract.Contacts.DISPLAY_NAME_PRIMARY :
-                    ContactsContract.Contacts.DISPLAY_NAME
-    };
-
-    @SuppressLint("InlinedApi")
-    private static final String[] PROJECTION =
-            {
-                    ContactsContract.Contacts._ID,
-                    ContactsContract.Contacts.LOOKUP_KEY,
-                    Build.VERSION.SDK_INT
-                            >= Build.VERSION_CODES.HONEYCOMB ?
-                            ContactsContract.Contacts.DISPLAY_NAME_PRIMARY :
-                            ContactsContract.Contacts.DISPLAY_NAME
-
-            };
     public addfriend_Fragment(){}
 
     @Override
@@ -95,30 +54,24 @@ public class addfriend_Fragment extends Fragment implements LoaderManager.Loader
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState){
 
+
         View view = inflater.inflate(R.layout.addfriend_fragment, container, false);
         mContactsList = view.findViewById(R.id.list);
 
-        /*mCursorAdapter = new SimpleCursorAdapter(
-                getActivity(),
-                R.id.theList,
-                null,
-                FROM_COLUMNS, TO_IDS,
-                0);
+        friends = retrieveContactName();
+        LinkedList<String> names = new LinkedList<>();
 
-        getLoaderManager().initLoader(0, null, this);
-        mContactsList.setAdapter(mCursorAdapter);
-        mContactsList.setOnItemClickListener(this);
-        */
+        for(int i=0;friends.size()> i; i++){
+            names.add( friends.get(i).getName());
+        }
 
-        String[] friends = {"Friend 1",
-                            "Friend 2",
-                            "Friend 3" };
         ArrayAdapter<String> listViewAdapter = new ArrayAdapter<String>(
                 getActivity(),
                 android.R.layout.simple_list_item_1,
-                friends
+                names
         );
         mContactsList.setAdapter(listViewAdapter);
+        mContactsList.setOnItemClickListener(this);
         return view;
     }
 
@@ -129,47 +82,35 @@ public class addfriend_Fragment extends Fragment implements LoaderManager.Loader
     @Override
     public void onItemClick(
             AdapterView<?> parent, View item, int position, long rowID) {
-        /* Get the Cursor */
-        Cursor cursor = ((SimpleCursorAdapter) parent.getAdapter()).getCursor();
-        // Move to the selected contact
-        cursor.moveToPosition(position);
-        // Get the _ID value
-        mContactId = cursor.getLong(CONTACT_ID_INDEX);
-        // Get the selected LOOKUP KEY
-        mContactKey = cursor.getString(CONTACT_KEY_INDEX);
-        // Create the contact's content Uri
-        mContactUri = ContactsContract.Contacts.getLookupUri(mContactId, mContactKey);
-        /*
-         * You can use mContactUri as the content URI for retrieving
-         * the details for a contact.
-         */
-    }
-    @Override
-    public Loader<Cursor> onCreateLoader(int loaderId, Bundle args) {
-        /*
-         * Makes search string into pattern and
-         * stores it in the selection array
-         */
-        mSelectionArgs[0] = "%" + mSearchString + "%";
-        // Starts the query
-        return new CursorLoader(
-                getActivity(),
-                ContactsContract.Contacts.CONTENT_URI,
-                PROJECTION,
-                SELECTION,
-                mSelectionArgs,
-                null
-        );
-    }
-    public void onLoadFinished(Loader<Cursor> loader, Cursor cursor) {
-        // Put the result Cursor in the adapter for the ListView
-        mCursorAdapter.swapCursor(cursor);
-    }
-    @Override
-    public void onLoaderReset(Loader<Cursor> loader) {
-        // Delete the reference to the existing Cursor
-        mCursorAdapter.swapCursor(null);
 
-    }
+        Log.d(TAG, "Name you clicked on:  "+friends.get(position).getName());
 
+}
+
+    private LinkedList<Friend> retrieveContactName() {
+
+        LinkedList<Friend> contacts = new LinkedList<Friend>();
+        ContentResolver cr = getActivity().getContentResolver();
+        // querying contact data store
+        String name;
+        String email = "fake@fake.fake";
+        Cursor cursor = getActivity().getContentResolver().query(ContactsContract.Contacts.CONTENT_URI, null, null, null, null);
+        if (cursor.getCount() > 0) {
+            while (cursor.moveToNext()) {
+                String id = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts._ID));
+                Cursor cur1 = cr.query(
+                        ContactsContract.CommonDataKinds.Email.CONTENT_URI, null,
+                        ContactsContract.CommonDataKinds.Email.CONTACT_ID + " = ?",
+                        new String[]{id}, null);
+                name = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
+                if (cur1.moveToNext()) {
+                    email = cur1.getString(cur1.getColumnIndex(ContactsContract.CommonDataKinds.Email.DATA));
+                }
+                contacts.add(new Friend(name,email));
+            }
+        }
+        cursor.close();
+
+        return contacts;
+    }
 }
